2152a7f64a890dd1fa41c04123276fe1c414af7c
Neeraj Vishwakarma <neeraj.vishwakarma@mozaiq.ai>
2024-03-29 04:09:22 +0530

    reset project

--- myprofile/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-# Create your views here.
-
-def home(request):
-    print("Hi -----")
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render())
